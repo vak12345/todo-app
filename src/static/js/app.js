8fa125bb1715ef2cc749f2d7f04a38771f014299
Haroon Khan <Haroon@gmial.com>
2023-09-22 10:36:14 +0000

    addeddd

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">You have no todo items yet! Add one above and enjoy</p>
+                <p className="text-center">You have no todo items yet! Add one above and enjoy!!!!</p>
             )}
             {items.map(item => (
                 <ItemDisplay
